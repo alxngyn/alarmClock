3deb28aa211b9bf8cc263232f5c7b893bab5f803
Alex Nguyen <alexnguyen@Alexs-MacBook-Pro-2.local>
2015-01-08 00:57:17 -0800

    removed stuff i didnt want

--- alarmClock.py
+++ alarmClock.py
@@ -5,10 +5,10 @@ import subprocess
 import time
 import textwrap
 from better_spoken_time3 import gmt, day
-from get_url_btc4 import btc
-from get_url_stocks8 import stck
+#from get_url_btc4 import btc
+#from get_url_stocks8 import stck
 from get_url_weather9 import wtr, frc
-from get_url_news8 import news
+#from get_url_news8 import news
 
 
 count = 1
@@ -22,18 +22,22 @@ head = 'wget -q -U Mozilla '
 tail = '.mp3 '
 
 # end
-end = 'Thats all for now.  Have a nice day.  ' 
+end = 'Thats all for now.  Have a nice day.  '
 
 # Turn all of the parts into a single string
-wad = (gmt + name + day + wtr + frc + btc + stck + news + end)
-# print wad
+#old wad
+#wad = (gmt + name + day + wtr + frc + btc + stck + news + end)
 
+wad = (gmt + name + day + wtr + frc + end)
+
+# print wad
+print "WAD = " + wad
 # strip any quotation marks
 wad = wad.replace('"', '').strip()
 wad = wad.replace("'", '').strip()
 
 # If you want to say with pure FOSS projects, use festival instead of google tts by uncommenting out the line below AND commenting out EVERYTHING else
-# print subprocess.check_output("echo " + wad + " | festival --tts ", shell=True) 
+# print subprocess.check_output("echo " + wad + " | festival --tts ", shell=True)
 
 # Google voice only accepts 100 characters or less, so split into chunks
 shorts = []
@@ -56,9 +60,7 @@ try:
 # festival is now called in case of error reaching Google
 except subprocess.CalledProcessError:
   print subprocess.check_output("echo " + wad + " | festival --tts ", shell=True)
-  
+
 # Cleanup any mp3 files created in this directory.
 print 'cleaning up now'
 print subprocess.call ('rm /mnt/ram/*.mp3', shell=True)
-
-
